733cadab49c06bcf6741ea4c648668f9c9429b24
shimul <shimul@gmail.com>
2022-03-01 11:51:36 +0600

    functional updated

--- api.js
+++ api.js
@@ -16,6 +16,8 @@ const searchMobile = () => {
 const displaySearchResult = mobiles => {
     console.log(mobiles);
     const searchResut = document.getElementById('search-result');
+
+    searchResut.textContent = '';
     mobiles.forEach(mobile => {
 
         const div = document.createElement('div');
@@ -35,7 +37,7 @@ const displaySearchResult = mobiles => {
                      </div>
                      <div class="button-container">
                      <button  class="delete-btn btn btn-danger">Delete</button>
-                     <button onclick="teamDetails()" class="details-btn btn btn-success">Details</button>
+                     <button onclick="loadPhonDetails('${mobile.slug}')" class="details-btn btn btn-success">Details</button>
                   </div>
                  </div>
                </div>
@@ -46,4 +48,50 @@ const displaySearchResult = mobiles => {
 
 
     })
+}
+const loadPhonDetails = mobileSlug => {
+    console.log(mobileSlug);
+    const url = ` https://openapi.programming-hero.com/api/phone/${mobileSlug}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhonDetails(data.data));
+
+
+}
+const displayPhonDetails = (data) => {
+    console.log(data);
+    const phonDetails = document.getElementById('phon-details');
+
+
+    phonDetails.textContent = '';
+
+    const div = document.createElement('div');
+
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="img rounded">
+    <img
+    class="w-35"
+    src="${data.image}"
+     alt="picture"
+      />
+    </div>
+    <div class="card-body">
+                
+                <h5 class="card-title">${data.brand}</h5>
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-title">${data.releaseDate}</p>
+                <p class="card-title">${data.mainFeatures.storage}</p>
+                <p class="card-title">${data.mainFeatures.displaySize}</p>
+                <p class="card-title">${data.mainFeatures.chipSet}</p>
+                <p class="card-title">${data.mainFeatures.memory}</p>
+                <h5 class="card-title">${data.slug}</h5>
+                
+                
+
+
+            </div>
+
+    `;
+    phonDetails.appendChild(div);
 }
